79bd3d042c67c48c4946c59110c0d0f52fe9509d
Jamil Ahmed <jaamil.syl@gmail.com>
2022-01-23 02:19:54 +0600

    Profile update and delete profile & user put experience and education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,7 @@ const { check, validationResult } = require("express-validator");
 const res = require("express/lib/response");
 const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 const router = express.Router();
 
 // @route....GET /api/profile/me
@@ -98,6 +99,144 @@ router.post(
 		} catch (err) {
 			console.error(err.message);
 			res.status(500).json("Internal Server Error");
+		}
+	}
+);
+
+// @route   GET /api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get("/", async (req, res) => {
+	try {
+		const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+
+		res.json(profiles);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Internal server error");
+	}
+});
+
+// @route   GET /api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+router.get("/user/:user_id", async (req, res) => {
+	console.log(req.params);
+	try {
+		const profile = await Profile.findOne({
+			user: req.params.user_id,
+		}).populate("user", ["name", "avatar"]);
+
+		if (!profile)
+			return res.status(400).json({ msg: "There is no user for this id" });
+
+		res.json(profile);
+	} catch (err) {
+		console.error(err.message);
+		if (err.kind == "ObjectId")
+			return res.status(400).json({ msg: "There is no user by this id" });
+		res.status(500).send("Internal server error");
+	}
+});
+
+// @route   DELETE /api/profile
+// @desc    Delete profile, user & posts
+// @access  Private
+router.delete("/", auth, async (req, res) => {
+	try {
+		// Remove profile
+		await Profile.findOneAndRemove({ user: req.user.id });
+
+		// Remove user
+		await User.findOneAndRemove({ _id: req.user.id });
+
+		res.json({ msg: "User removed successfully" });
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Internal server error");
+	}
+});
+
+// @route   PUT /api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put(
+	"/experience",
+	[
+		auth,
+		check("title", "Title is required").not().isEmpty(),
+		check("company", "Company is required").not().isEmpty(),
+		check("from", "From date is required").not().isEmpty(),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { title, company, location, from, to, current, description } =
+			req.body;
+
+		const newExp = {
+			title,
+			company,
+			location,
+			from,
+			to,
+			current,
+			description,
+		};
+
+		try {
+			const profile = await Profile.findOne({ user: req.user.id });
+			profile.experience.unshift(newExp);
+			await profile.save();
+
+			res.json(profile);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Internal server error");
+		}
+	}
+);
+
+// @route   PUT /api/profile/education
+// @desc    Add profile education
+// @access  Private
+router.put(
+	"/education",
+	[
+		auth,
+		check("school", "School is required").not().isEmpty(),
+		check("degree", "Degree is required").not().isEmpty(),
+		check("from", "From date is required").not().isEmpty(),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { school, degree, from, to, current, description } = req.body;
+
+		const newEdu = {
+			school,
+			degree,
+			from,
+			to,
+			current,
+			description,
+		};
+
+		try {
+			const profile = await Profile.findOne({ user: req.user.id });
+			profile.education.unshift(newEdu);
+			await profile.save();
+
+			res.json(profile);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Internal server error");
 		}
 	}
 );
