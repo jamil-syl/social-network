1dfe0d0c32b18162cccb1e1ce35fe372e6ccd78c
Jamil Ahmed <jaamil.syl@gmail.com>
2022-01-25 00:20:05 +0600

    Delete experience and education create post and update

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,56 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+const auth = require("../../middleware/auth");
 
-// @route....GET /api/posts
-// @des...Tst route
-// @access...public
-router.get('/', (req, res) => res.send("User posts"));
+// @route. Post /api/posts
+// @des... Create a post
+// @access Private
+router.post(
+	"/",
+	[auth, [check("text", "Text is required").not().isEmpty()]],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		try {
+			const user = await User.findById(req.user.id).select("-password");
+			const newPost = new Post({
+				text: req.body.text,
+				name: user.name,
+				avatar: user.avatar,
+				user: req.user.id,
+			});
+			const post = await newPost.save();
+			res.json(post);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Internal Server Error");
+		}
+	}
+);
+
+// @route. Put /api/post/:post_id
+// @des... Update a post
+// @access Private
+router.put("/post_id", auth, async (req, res) => {
+	try {
+		const updatePost = await Post.findByIdAndUpdate(
+			req.params.post_id,
+			{
+				$set: req.body,
+			},
+			{ new: true }
+		);
+		res.json(updatePost);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Internal Server Error");
+	}
+});
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -200,6 +200,28 @@ router.put(
 	}
 );
 
+// @route   DELETE /api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+	try {
+		const profile = await Profile.findOne({ user: req.user.id });
+
+		// Get remove index
+		const removeIndex = profile.experience
+			.map((item) => item.id)
+			.idexOf(req.params.exp_id);
+
+		if (removeIndex == 0) profile.experience.splice(removeIndex, 1);
+
+		await profile.save();
+		res.json(profile);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Internal Server Error");
+	}
+});
+
 // @route   PUT /api/profile/education
 // @desc    Add profile education
 // @access  Private
@@ -217,11 +239,13 @@ router.put(
 			return res.status(400).json({ errors: errors.array() });
 		}
 
-		const { school, degree, from, to, current, description } = req.body;
+		const { school, degree, fieldofstudy, from, to, current, description } =
+			req.body;
 
 		const newEdu = {
 			school,
 			degree,
+			fieldofstudy,
 			from,
 			to,
 			current,
@@ -240,5 +264,27 @@ router.put(
 		}
 	}
 );
+
+// @route   DELETE /api/profile/education/:edu_id
+// @desc    Delete education from profile
+// @access  Private
+router.delete("/education/:edu_id", auth, async (req, res) => {
+	try {
+		const profile = await Profile.findOne({ user: req.user.id });
+
+		// Get remove index
+		const removeIndex = profile.education
+			.map((item) => item.id)
+			.idexOf(req.params.edu_id);
+
+		if (removeIndex == 0) profile.education.splice(removeIndex, 1);
+
+		await profile.save();
+		res.json(profile);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Internal Server Error");
+	}
+});
 
 module.exports = router;
